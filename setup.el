52a6e9e149504ee0f938af61258f99be812ed6cd
mikaelpoul <mikajoh@gmail.com>
2015-07-30 12:27:32 +0200

    added markdown major-mode

--- setup.el
+++ setup.el
@@ -35,4 +35,10 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-
+;; Markdown mode
+(add-to-list 'load-path "~/.emacs.d/plugins/markdown-mode")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))

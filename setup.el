5972eb51344a83f9359a14d3a50e40eb3e540169
mikaelpoul <mikajoh@gmail.com>
2015-07-31 19:05:58 +0200

    added org-mode

--- setup.el
+++ setup.el
@@ -109,3 +109,12 @@
 
 ;; Do not show welcome screen
 (setq inhibit-startup-message t)
+
+;; Org-mode
+(add-to-list 'load-path "~/.emacs.d/plugins/orgdir/lisp")
+(add-to-list 'load-path "~/.emacs.d/plugins/orgdir/contrib/lisp" t)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
